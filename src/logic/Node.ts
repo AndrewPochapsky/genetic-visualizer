8a0b0a53b3e92b6a2c665191bd7d605f08be9a40
AndrewPochapsky <andrew.pochapsky@gmail.com>
2020-07-11 15:49:13 -0400

    started working on node class

---
+++ src/logic/Node.ts
@@ -0,0 +1,20 @@
+export default class Node {
+  private colorVector: [number, number, number];
+  constructor() {
+    // Initialize to white.
+    this.colorVector = [255, 255, 255];
+  }
+
+  // Used as the fitness in this case, smaller number -> more fit.
+  getDistanceFromTarget(targetColor: [number, number, number]): number {
+    let difference = [0, 0, 0];
+    for (let i = 0; i < 3; i++) {
+      difference[i] = targetColor[i] - this.colorVector[i];
+    }
+    return Math.sqrt(
+      difference.reduce((result, item) => {
+        return result + Math.pow(item, 2);
+      })
+    );
+  }
+}
