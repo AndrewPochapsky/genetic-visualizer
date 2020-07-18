98b3b1695a85938754d7af0df6642c416566b242
AndrewPochapsky <andrew.pochapsky@gmail.com>
2020-07-18 19:11:00 -0400

    added comments and cleaned things up

--- src/components/GeneticVisualizer.tsx
+++ src/components/GeneticVisualizer.tsx
@@ -3,7 +3,6 @@ import Header from "./Header";
 import GeneticNode from "../logic/GeneticNode";
 import { SettingsState, PopulationSize, MutationType } from "./Settings";
 import Node from "./node/Node";
-import "./GeneticVisualizer.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -35,7 +34,12 @@ export default class GeneticVisualizer extends React.Component<
     });
   }
 
-  nextGeneration = (times = 1) => {
+  /**
+   * Entry point for gettting the next generation of nodes.
+   *
+   * @param times  The number of generations to acquire before updating grid
+   */
+  public nextGeneration = (times = 1) => {
     let grid = this.state.grid;
     let flattenedNodes = this.getFlattenedNodes(grid);
     let generationNumber = this.state.generationNumber;
@@ -45,6 +49,7 @@ export default class GeneticVisualizer extends React.Component<
         this.props.settings.endColor,
         this.props.settings.matingPoolPercent
       );
+      // Keeps the population size constant.
       let numCrossovers = flattenedNodes.length - matingPool.length;
       flattenedNodes = this.getNextPopulation(
         matingPool,
@@ -60,6 +65,28 @@ export default class GeneticVisualizer extends React.Component<
     });
   };
 
+  /**
+   * Sorts the nodes in the grid by their fitness.
+   */
+  public sortByFitness = () => {
+    let endColor = this.props.settings.endColor;
+    let flattenedNodes = this.getFlattenedNodes(this.state.grid);
+    GeneticNode.sortByFitness(flattenedNodes, endColor);
+    this.setState({
+      grid: this.unFlattenNodes(flattenedNodes),
+    });
+  };
+
+  /**
+   * Gets the next population of nodes by creating offspring from the given
+   * mating pool.
+   *
+   * @param matingPool  The genetic nodes to get parents from
+   * @param numCrossovers  The number of cross overs to perform
+   * @param generationNumber  The generation number
+   *
+   * @returns The new population
+   */
   private getNextPopulation(
     matingPool: GeneticNode[],
     numCrossovers: number,
@@ -69,29 +96,48 @@ export default class GeneticVisualizer extends React.Component<
     for (let i = 0; i < numCrossovers; i++) {
       let parents = this.getParents(matingPool);
       let offspring = GeneticNode.crossover(parents[0], parents[1]);
-      let mutationChance = 0;
-      switch (this.props.settings.mutationType) {
-        case MutationType.Gradual: {
-          mutationChance = offspring.mutateGradual(
-            this.props.settings.decreaseMutation ? generationNumber : -1,
-            this.props.settings.mutationChance
-          );
-          break;
-        }
-        case MutationType.Invert: {
-          mutationChance = offspring.mutateInvert(
-            this.props.settings.decreaseMutation ? generationNumber : -1,
-            this.props.settings.mutationChance
-          );
-          break;
-        }
-      }
+      let mutationChance = this.mutateOffspring(offspring, generationNumber);
       this.setState({ mutationChance: mutationChance });
       nextPopulation.push(offspring);
     }
     return nextPopulation;
   }
 
+  /**
+   * Mutates the given offspring based on mutationType.
+   *
+   * @param offspring  The new genetic node
+   * @param generationNumber  The generation number
+   *
+   * @returns The updated mutation chance
+   */
+  private mutateOffspring(
+    offspring: GeneticNode,
+    generationNumber: number
+  ): number {
+    switch (this.props.settings.mutationType) {
+      case MutationType.Gradual: {
+        return offspring.mutateGradual(
+          this.props.settings.decreaseMutation ? generationNumber : -1,
+          this.props.settings.mutationChance
+        );
+      }
+      case MutationType.Invert: {
+        return offspring.mutateInvert(
+          this.props.settings.decreaseMutation ? generationNumber : -1,
+          this.props.settings.mutationChance
+        );
+      }
+    }
+  }
+
+  /**
+   * Retrieves two random parents from the given mating pool.
+   *
+   * @param matingPool  The mating pool to choose parents from
+   *
+   * @returns The two chosen parents in an array of length 2
+   */
   private getParents(matingPool: GeneticNode[]): [GeneticNode, GeneticNode] {
     let index1 = Math.floor(Math.random() * matingPool.length);
     let index2 = Math.floor(Math.random() * matingPool.length);
@@ -101,24 +147,82 @@ export default class GeneticVisualizer extends React.Component<
     return [matingPool[index1], matingPool[index2]];
   }
 
-  private getFlattenedNodes(geneticNodes: GeneticNode[][]): GeneticNode[] {
+  /**
+   * Converts the 2d array of genetic nodes into a 1d array of genetic nodes.
+   *
+   * @param nodes  The nodes to flatten
+   *
+   * @returns The flattened nodes
+   */
+  private getFlattenedNodes(nodes: GeneticNode[][]): GeneticNode[] {
     let flattenedNodes: GeneticNode[] = [];
-    for (let i = 0; i < geneticNodes.length; i++) {
-      for (let j = 0; j < geneticNodes[i].length; j++) {
-        flattenedNodes.push(geneticNodes[i][j]);
+    for (let i = 0; i < nodes.length; i++) {
+      for (let j = 0; j < nodes[i].length; j++) {
+        flattenedNodes.push(nodes[i][j]);
       }
     }
     return flattenedNodes;
   }
 
-  sortByFitness = () => {
-    let endColor = this.props.settings.endColor;
-    let flattenedNodes = this.getFlattenedNodes(this.state.grid);
-    GeneticNode.sortByFitness(flattenedNodes, endColor);
-    this.setState({
-      grid: this.unFlattenNodes(flattenedNodes),
-    });
+  /**
+   * Converts 1d array of genetic nodes into a 2d array of genetic nodes
+   * into the size it was initially set to.
+   *
+   * @param nodes  The nodes to unflatten
+   *
+   * @returns The nodes in a 2d array
+   */
+  private unFlattenNodes(nodes: GeneticNode[]): GeneticNode[][] {
+    let size = this.state.grid.length;
+    let index = 0;
+    let grid = [];
+    for (let i = 0; i < size; i++) {
+      let currentRow = [];
+      for (let j = 0; j < size; j++) {
+        currentRow.push(nodes[index]);
+        index++;
+      }
+      grid.push(currentRow);
+    }
+    return grid;
+  }
+
+  /**
+   * Gets the initial grid.
+   *
+   * @returns The created grid of genetic nodes
+   */
+  private getInitialGrid = (): GeneticNode[][] => {
+    let size = this.getGridSize();
+    const grid = [];
+    for (let row = 0; row < size; row++) {
+      const currentRow = [];
+      for (let col = 0; col < size; col++) {
+        currentRow.push(new GeneticNode());
+      }
+      grid.push(currentRow);
+    }
+    return grid;
   };
+
+  /**
+   * Gets the grid size based on the chosen populationSize.
+   *
+   * @returns The grid size
+   */
+  private getGridSize(): number {
+    switch (this.props.settings.populationSize) {
+      case PopulationSize.Small: {
+        return 10;
+      }
+      case PopulationSize.Medium: {
+        return 20;
+      }
+      case PopulationSize.Large: {
+        return 36;
+      }
+    }
+  }
 
   render() {
     return (
@@ -152,46 +256,4 @@ export default class GeneticVisualizer extends React.Component<
       </div>
     );
   }
-
-  unFlattenNodes(nodes: GeneticNode[]): GeneticNode[][] {
-    let size = this.state.grid.length;
-    let index = 0;
-    let grid = [];
-    for (let i = 0; i < size; i++) {
-      let currentRow = [];
-      for (let j = 0; j < size; j++) {
-        currentRow.push(nodes[index]);
-        index++;
-      }
-      grid.push(currentRow);
-    }
-    return grid;
-  }
-
-  getInitialGrid = () => {
-    let size = 0;
-    switch (this.props.settings.populationSize) {
-      case PopulationSize.Small: {
-        size = 10;
-        break;
-      }
-      case PopulationSize.Medium: {
-        size = 20;
-        break;
-      }
-      case PopulationSize.Large: {
-        size = 36;
-        break;
-      }
-    }
-    const grid = [];
-    for (let row = 0; row < size; row++) {
-      const currentRow = [];
-      for (let col = 0; col < size; col++) {
-        currentRow.push(new GeneticNode());
-      }
-      grid.push(currentRow);
-    }
-    return grid;
-  };
 }
--- src/logic/GeneticNode.ts
+++ src/logic/GeneticNode.ts
@@ -1,3 +1,6 @@
+/**
+ * The individuals in the population.
+ */
 export default class GeneticNode {
   colorVector: [number, number, number];
   constructor() {
@@ -8,6 +11,11 @@ export default class GeneticNode {
   /**
    * Offspring's colorVector determined by linear interpolation between both
    * parents' color vectors. This is done for each color channel.
+   *
+   * @param parent1  The first parent
+   * @param parent2  The second parent
+   *
+   * @returns The offspring of the two parents
    */
   static crossover(parent1: GeneticNode, parent2: GeneticNode): GeneticNode {
     let offspring = new GeneticNode();
@@ -20,23 +28,32 @@ export default class GeneticNode {
     return offspring;
   }
 
+  /**
+   * Selects the top matingPoolPercent of the population based on fitness.
+   *
+   * @param nodes  The genetic nodes to select from
+   * @param targetColor  The color to optimize for
+   * @param matingPoolPercent  The percent of population chosen for mating
+   *
+   * @returns The nodes that will be used for mating
+   */
   static getMatingPool(
-    flattenedNodes: GeneticNode[],
+    nodes: GeneticNode[],
     targetColor: [number, number, number],
     matingPoolPercent: number
   ): GeneticNode[] {
-    this.sortByFitness(flattenedNodes, targetColor);
-    return flattenedNodes.slice(
-      0,
-      Math.round(flattenedNodes.length * matingPoolPercent)
-    );
+    this.sortByFitness(nodes, targetColor);
+    return nodes.slice(0, Math.round(nodes.length * matingPoolPercent));
   }
 
+  /**
+   * Sorts the nodes by fitness, decsecnding.
+   */
   static sortByFitness(
-    flattenedNodes: GeneticNode[],
+    nodes: GeneticNode[],
     targetColor: [number, number, number]
   ) {
-    flattenedNodes.sort((a, b) =>
+    nodes.sort((a, b) =>
       a.getDistanceFromTarget(targetColor) <
       b.getDistanceFromTarget(targetColor)
         ? -1
@@ -44,7 +61,15 @@ export default class GeneticNode {
     );
   }
 
-  // Used as the fitness in this case, smaller number -> more fit.
+  /**
+   * Used to determine the fitness of an individual by computing the
+   * euclidean distance between the node's color vector and the target color
+   * vector. Smaller number implies more fit.
+   *
+   * @param targetColor  The color to optimize for
+   *
+   * @returns The euclidean distance between this.colorVector and targetColor
+   */
   getDistanceFromTarget(targetColor: [number, number, number]): number {
     let difference = [0, 0, 0];
     for (let i = 0; i < 3; i++) {
@@ -57,6 +82,14 @@ export default class GeneticNode {
     );
   }
 
+  /**
+   * Mutation function that adds a value between -50 and 50 to a color channel.
+   *
+   * @param generationNumber  The generation number
+   * @param mutationChance  The chance of mutation
+   *
+   * @returns The updated mutation chance
+   */
   mutateGradual(generationNumber: number, mutationChance: number): number {
     return this.mutate(
       generationNumber,
@@ -65,6 +98,14 @@ export default class GeneticNode {
     );
   }
 
+  /**
+   * Mutation function that inverts a color channel.
+   *
+   * @param generationNumber  The generation number
+   * @param mutationChance  The chance of mutation
+   *
+   * @returns The updated mutation chance
+   */
   mutateInvert(generationNumber: number, mutationChance: number): number {
     return this.mutate(
       generationNumber,
@@ -73,6 +114,15 @@ export default class GeneticNode {
     );
   }
 
+  /**
+   * Executes mutation function onto each colour channel if a mutation occurs.
+   *
+   * @param generationNumber  The generation number
+   * @param mutationChance  The chance of mutation
+   * @param mutation  The function to be used for mutation
+   *
+   * @returns The updated mutation chance
+   */
   private mutate(
     generationNumber: number,
     mutationChance: number,
@@ -94,16 +144,34 @@ export default class GeneticNode {
     return mutationChance;
   }
 
+  /**
+   * Gets a pseudo random integer between the given range.
+   *
+   * @param min  The lower bound inclusive
+   * @param max The upper bound inclusive
+   *
+   * @returns The pseudo random number
+   */
   private getRandomInt(min: number, max: number): number {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  /*
+  /**
    * As the generation number goes up the mutation chance should be decreased.
+   * If generationNumber is -1 then it is implied that the user doesn't want to
+   * lower the mutation value as generation increase.
+   *
+   * @param mutationChance The chance of mutation
+   * @param generationNumber  The generation number
+   *
+   * @returns The updated mutation chance
    */
-  getMutationChance(mutationChance: number, generationNumber: number) {
+  private getMutationChance(
+    mutationChance: number,
+    generationNumber: number
+  ): number {
     if (generationNumber !== -1) {
       return mutationChance / generationNumber;
     }
